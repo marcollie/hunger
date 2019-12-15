5a34ccac8c3123fbd33d844af8e64f7831e2caa4
marcollie <imwhoa@naver.com>
2019-12-15 16:07:46 +0900

    gallery-slide-repons

--- js/mobileSl.js
+++ js/mobileSl.js
@@ -1,19 +1,24 @@
-let index = 0;
-const slidebox = document.querySelector('.gallery-box');
-const firstitem = document.querySelector('.galImg');
-const firstClone = firstitem.cloneNode(true);
-slidebox.appendChild(firstClone);
+$(window).resize(function () {
+  if ($(window).width() <= 414) {
+    let index = 0;
+    const slidebox = document.querySelector('.gallery-box');
+    const firstitem = document.querySelector('.galImg');
+    const firstClone = firstitem.cloneNode(true);
+    slidebox.appendChild(firstClone);
 
-setInterval(function () {
-  slidebox.style.transition = '0.2s';
-  slidebox.style.transform = "translate3d(-" + 20 * (index + 1) + "%, 0px, 0px)";
+    setInterval(function () {
+      slidebox.style.transition = '0.2s';
+      slidebox.style.transform = "translate3d(-" + 20 * (index + 1) + "%, 0px, 0px)";
 
-  index++;
-  if (index === 4) {
-    setTimeout(function () {
-      slidebox.style.transition = '0s';
-      slidebox.style.transform = "translate3d(0px, 0px, 0px)";
-    }, 200)
-    index = 0;
+      index++;
+      if (index === 4) {
+        setTimeout(function () {
+          slidebox.style.transition = '0s';
+          slidebox.style.transform = "translate3d(0px, 0px, 0px)";
+        }, 200)
+        index = 0;
+      }
+    }, 1500);
+
   }
-}, 1500);
+});
